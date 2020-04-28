5409cc5e828668a30cf96516b0a5cea4b852e385
kunxin-chor <chorkunxin@yahoo.com>
2020-04-28 06:52:11 +0000

    Added pagination example

--- airbnb/app.py
+++ airbnb/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
+import math
 import datetime
 import pymongo
 from bson.objectid import ObjectId
@@ -8,12 +9,26 @@ load_dotenv()
 
 
 MONGO_URI = os.environ.get('MONGO_URI')
-DB_NAME = "animal_shelter"
+DB_NAME = "sample_airbnb"
 
 # Creates a mongo client
 client = pymongo.MongoClient(MONGO_URI)
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+
+    records_per_page = 10
+    max_pages = math.ceil(client[DB_NAME]['listingsAndReviews'].count() / records_per_page)
+
+    current_page = int(request.args.get('current_page', 1))
+
+
+    # only show the first ten
+    listings = client[DB_NAME]['listingsAndReviews'].find({}).skip((current_page-1)*records_per_page).limit(records_per_page)
+    return render_template('index.template.html', results=listings, max_pages=max_pages, current_page=current_page)
 
 
 # "magic code" -- boilerplate
